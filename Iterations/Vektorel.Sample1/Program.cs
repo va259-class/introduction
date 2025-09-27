377e6cb8be19f1b74692128c66fa8f4d81b86fd1
va259-class <va259@outlook.com>
2025-09-27 12:07:21 +0300

    Döngü ile meyve alım işlemi

--- Iterations/Vektorel.Sample1/Program.cs
+++ Iterations/Vektorel.Sample1/Program.cs
@@ -4,38 +4,23 @@
     {
         static void Main(string[] args)
         {
+            Console.Write("Sepetteki Meyve Çeşidi Adedi...: ");
+            var count = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("=================================");
             var total = 0D;
-            Fruit f1 = new Fruit();
-            Console.Write("Meyve Giriniz...........: ");
-            f1.name = Console.ReadLine();
-            Console.Write("Alım Miktarı Giriniz....: ");
-            f1.amount = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Fiyatı Giriniz..........: ");
-            f1.price = Convert.ToDouble(Console.ReadLine());
 
-            total += f1.GetTotal();
-            Console.WriteLine();
-
-            Fruit f2 = new Fruit();
-            Console.Write("Meyve Giriniz...........: ");
-            f2.name = Console.ReadLine();
-            Console.Write("Alım Miktarı Giriniz....: ");
-            f2.amount = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Fiyatı Giriniz..........: ");
-            f2.price = Convert.ToDouble(Console.ReadLine());
-            
-            total += f2.GetTotal();
-            Console.WriteLine();
-
-            Fruit f3 = new Fruit();
-            Console.Write("Meyve Giriniz...........: ");
-            f3.name = Console.ReadLine();
-            Console.Write("Alım Miktarı Giriniz....: ");
-            f3.amount = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Fiyatı Giriniz..........: ");
-            f3.amount = Convert.ToDouble(Console.ReadLine());
-
-            total += f3.GetTotal();
+            for (int i = 0; i < count; i++)
+            {
+                Fruit f = new Fruit();
+                Console.Write("Meyve Giriniz...........: ");
+                f.name = Console.ReadLine();
+                Console.Write("Alım Miktarı Giriniz....: ");
+                f.amount = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Fiyatı Giriniz..........: ");
+                f.price = Convert.ToDouble(Console.ReadLine());
+                total += f.GetTotal();
+                Console.WriteLine();
+            }
 
             Console.WriteLine("Toplam Fiyat " + total);
         }

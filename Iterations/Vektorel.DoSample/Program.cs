ec3126aae4d43a0165c743dc02c600872658cc45
va259-class <va259@outlook.com>
2025-09-27 13:29:49 +0300

    Do döngüsü ile OOP örneği

---
+++ Iterations/Vektorel.DoSample/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Vektorel.DoSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //HardWorker mesaj numarasını 650 - 700 arasında vermezse
+            //ilgili işlem yapılamaz.
+            // COOL YÖNTEM
+            var hw = new HardWorker();
+            var value = 0;
+            do
+            {
+                if (value != 0) // id 0 değilse
+                {
+                    Console.WriteLine("Referans yeniden kontrol ediliyor");
+                }
+                Thread.Sleep(300);
+                value = hw.GetReferenceValue();
+                Console.WriteLine("Referans Değeri : " + value);
+            } while (hw.IsOutOfReference(value));
+            hw.DoWork();
+
+            //-----------------------------------------------
+            // KEKO YÖNTEM
+            while (true)
+            {
+                var id = Random.Shared.Next(1, 1000);
+                Console.WriteLine("Referans değeri " + id);
+                if(id > 650 && id < 700)
+                {
+                    Console.WriteLine("İşlem başladı");
+                    Thread.Sleep(1500);// Gerçek manada çalışan bir iş 
+                    Console.WriteLine("İşlem tamamlandı");
+                    break;
+                }
+                Thread.Sleep(300);
+                Console.WriteLine("İşlem yeniden kontrol ediliyor");
+            }
+        }
+    }
+
+    class HardWorker
+    {
+        public int GetReferenceValue()
+        {
+            return Random.Shared.Next(1, 1000); // Rasgele sayı üret
+        }
+
+        public bool IsOutOfReference(int value)
+        {
+            return value < 650 || value > 700;
+        }
+
+        public void DoWork()
+        {
+            Console.WriteLine("İşlem başladı");
+            //miş gibi yapılan iş
+            Thread.Sleep(1500); // 1.5 saniye uygulamayı duraklat
+            Console.WriteLine("İşlem tamamlandı");
+        }
+    }
+}
---
+++ Iterations/Vektorel.LoopKinds/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Vektorel.LoopKinds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For Döngüsü ile:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            Console.WriteLine("While Döngüsü ile:");
+            int wi = 0;
+            while (wi < 10)
+            {
+                Console.WriteLine(wi);
+                wi++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Do Döngüsü ile:");
+            var di = 0;
+            do
+            {
+                Console.WriteLine(di);
+            } while (++di < 10);
+
+            Console.WriteLine("ForEach Döngüsü ile:");
+            Console.WriteLine("to be continued...");
+
+            var c = 4;
+            var d = c++ + 4;
+            var e = ++d + 3;
+
+            var b = e * d;
+            // B = 108 çıkar... Teyit Et!!!
+        }
+    }
+}

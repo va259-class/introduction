42cbeb76cd12d8073f211a10c30b8cc57e096e34
va259-class <va259@outlook.com>
2025-11-02 13:30:54 +0300

    abstractions

--- InheritanceAndInterfaces/Vektorel.BaseParentCorrelation/Vektorel.AbstractionSample/Program.cs
+++ InheritanceAndInterfaces/Vektorel.BaseParentCorrelation/Vektorel.AbstractionSample/Program.cs
@@ -4,7 +4,73 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Rectangle();
+            Console.WriteLine($"{r.KnownName} için alan : {r.GetArea()}");
+
+            var c = new Circle();
+            Console.WriteLine($"{c.KnownName} için alan : {c.GetArea()}");
         }
     }
+
+    interface IPerimeter
+    {
+        public double GetPerimeter(); // Çevre
+    }
+
+    abstract class Shape
+    {
+        public string KnownName { get; set; }
+        public abstract double GetArea();
+    }
+
+    sealed class Rectangle : Shape, IPerimeter
+    {
+        public Rectangle()
+        {
+            KnownName = "Dikdörtgen";
+        }
+        public double LongEdge { get; set; }
+        public double ShortEdge { get; set; }
+
+        public override double GetArea()
+        {
+            return LongEdge * ShortEdge;
+        }
+
+        public double GetPerimeter()
+        {
+            return 2 * (LongEdge + ShortEdge);
+        }
+    }
+
+    class Square : Shape
+    {
+        public Square()
+        {
+            KnownName = "Kare";
+        }
+
+        public double Edge { get; set; }
+
+        public override double GetArea()
+        {
+            return Math.Pow(Edge, 2);
+        }
+    }
+
+    sealed class Circle : Shape
+    {
+        public Circle()
+        {
+            KnownName = "Çember";
+        }
+
+        public double Radius { get; set; }
+
+        public override double GetArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+
 }

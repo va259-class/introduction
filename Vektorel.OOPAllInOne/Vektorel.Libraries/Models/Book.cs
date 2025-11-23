99a437343bb6af589db05f56c6961bad26882ec4
va259-class <va259@outlook.com>
2025-11-23 11:33:40 +0300

    Tekrar 1

---
+++ Vektorel.OOPAllInOne/Vektorel.Libraries/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿namespace Vektorel.Libraries.Models
+{
+    public class Book
+    {
+        public int PageCount { get; set; }
+        public string Title { get; set; }
+        public Author Author { get; set; }
+
+        public string GetIntroduction()
+        {
+            return $"{Author.Name} kaleminden yazılan {Title} kitabı {PageCount} sayfadır";
+        }
+    }
+
+    public class Author
+    {
+        public Author(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; }
+    }
+}
---
+++ Vektorel.OOPAllInOne/Vektorel.OOPAllInOne/Program.cs
@@ -0,0 +1,19 @@
+﻿using Vektorel.Libraries.Models;
+
+namespace Vektorel.OOPAllInOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a1 = new Author("Fyodor Dostoyevski");
+            
+            var b1 = new Book();
+            b1.Title = "Karamazov Kardeşler";
+            b1.Author = a1;
+            b1.PageCount = 892;
+
+            Console.WriteLine(b1.GetIntroduction());
+        }
+    }
+}
